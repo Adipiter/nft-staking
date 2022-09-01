51f4c7a1f7ae8bb7d5c4ad73ace3a42bcec543a1
Adipiter <miuprix@gmail.com>
2022-09-01 10:06:53 +0700

    reconfig
    Signed-off-by: Adipiter <miuprix@gmail.com>

--- src/utils/contracts-config.js
+++ src/utils/contracts-config.js
@@ -1,7 +1,7 @@
 export const stakingContractAddress =
-  "0xC25a2d0c2F4eE6CC5f74Bd3d0ECeaC51932BDBb6";
-export const nftContractAddress = "0x033e0a2A0E283ffDC73ffe4DBB2608789319b07b";
+  "0x7406eccd8A773fC1CF0a7776918204B272fad256";
+export const nftContractAddress = "0xa171a42E930897ec916f8E1F7E8B917D83C122aB";
 export const tokenContractAddress =
-  "0xc24d584D1aBA591B5A8F41d54d1fcA3bb0dB99bd";
-export const ownerAddress = "0xfEa552d9fdf2C2372Ff6046463b01a72B1d9f941";
-export const networkDeployedTo = "56";
+  "0xe7a1a2A5D8F0bb925c94526F9A99C0c8E42f3A59";
+export const ownerAddress = "0xCeF8d179A4a7B62212c3D9140f8b56cBAE497Fa2";
+export const networkDeployedTo = "80001";
