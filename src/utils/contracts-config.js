6e745ed8dcc5d5e0d7fd7239aba06594b84f103d
Adipiter <miuprix@gmail.com>
2022-09-01 09:44:34 +0700

    Change network
    Signed-off-by: Adipiter <miuprix@gmail.com>

--- src/utils/contracts-config.js
+++ src/utils/contracts-config.js
@@ -4,4 +4,4 @@ export const nftContractAddress = "0x033e0a2A0E283ffDC73ffe4DBB2608789319b07b";
 export const tokenContractAddress =
   "0xc24d584D1aBA591B5A8F41d54d1fcA3bb0dB99bd";
 export const ownerAddress = "0xfEa552d9fdf2C2372Ff6046463b01a72B1d9f941";
-export const networkDeployedTo = "1337";
+export const networkDeployedTo = "56";
